8053de841e179b335a814f347ff04957d8afba24
sayeda050 <136953982+sayeda050@users.noreply.github.com>
2023-06-18 12:25:44 +0600

    added recursion

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include "splash.h"
+
+
+int main (void)
+{
+    recursiveLoading(0);
+    return 0;
+}
---
+++ splash.h
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<dos.h>
+
+
+void recursiveLoading(int progress) {
+    //Loading bar
+    // Base case: stop recursion when progress reaches 100%
+    if (progress > 100) {
+        return;
+    }
+
+    // Code to be executed in each iteration
+    printf("Project is Loading [%d%%]", progress);
+    fflush(stdout);
+    usleep(10000);
+    printf("\r");
+
+    // Recursive call to the function with an incremented progress value
+    recursiveLoading(progress + 1);
+}
